7221b81f8b57cf857be9d2a87a72deeddc800786
lucas_almeida <lucas.almdiniz@gmail.com>
2022-10-29 03:12:26 -0300

    test: Requisito 9:
    Testar a geração de uma versão do relatório em cores

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,26 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.importer.json_importer import JsonImporter
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+    path = 'inventory_report/data/inventory.json'
+    product_list = JsonImporter.import_data(path)
+    coloredReport = ColoredReport(SimpleReport)
+    report = coloredReport.generate(product_list)
+    splited_report = report.split('\n')
+
+    phrases = [
+        "\033[32mData de fabricação mais antiga:\033[0m ",
+        "\033[32mData de validade mais próxima:\033[0m ",
+        "\033[32mEmpresa com mais produtos:\033[0m ",
+    ]
+
+    value_phrases = [
+        "\033[36m2020-09-06\033[0m",
+        "\033[36m2023-09-17\033[0m",
+        "\033[31mTarget Corporation\033[0m"
+    ]
+
+    for index, row in enumerate(splited_report):
+        assert row == phrases[index] + value_phrases[index]

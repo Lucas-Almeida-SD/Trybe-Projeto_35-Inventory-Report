e8fbf8ae313103c15cfd4ad46a3ac79427a8f130
lucas_almeida <lucas.almdiniz@gmail.com>
2022-10-29 00:06:03 -0300

    feat: Requisito 6: criado método "read_json_file" e "read_file"
    Gere os relatórios através de um arquivo JSON

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,18 +1,16 @@
 from abc import ABC
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
-REPORT = {
-    'simples': SimpleReport,
-    'completo': CompleteReport
-}
+REPORT = {"simples": SimpleReport, "completo": CompleteReport}
 
 
 class Inventory(ABC):
     @classmethod
     def import_data(self, path, report_type):
-        product_list = Inventory.read_csv_file(path)
+        product_list = Inventory.read_file(path)
 
         return REPORT[report_type].generate(product_list)
 
@@ -22,9 +20,24 @@ class Inventory(ABC):
             reader = csv.reader(file)
             header, *data = reader
 
-        product_list = [
-            dict(zip(header, row))
-            for row in data
-        ]
+        product_list = [dict(zip(header, row)) for row in data]
 
         return product_list
+
+    @classmethod
+    def read_json_file(self, path):
+        with open(path) as file:
+            product_list = json.load(file)
+
+        return product_list
+
+    @classmethod
+    def read_file(self, path):
+        file_type = path.split(".")[-1]
+
+        readers = {
+            "csv": Inventory.read_csv_file,
+            "json": Inventory.read_json_file,
+        }
+
+        return readers[file_type](path)

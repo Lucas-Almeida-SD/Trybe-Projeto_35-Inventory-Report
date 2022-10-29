7decd43e28fc6ca0ee44c9c31153798a953c6dc7
lucas_almeida <lucas.almdiniz@gmail.com>
2022-10-29 00:17:55 -0300

    feat: Requisito 7: criado método "read_xml_file"
    Gere os relatórios através de um arquivo XML

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,6 +1,7 @@
 from abc import ABC
 import csv
 import json
+import xmltodict
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -32,12 +33,20 @@ class Inventory(ABC):
         return product_list
 
     @classmethod
+    def read_xml_file(self, path):
+        with open(path) as file:
+            product_list = xmltodict.parse(file.read())['dataset']['record']
+
+        return product_list
+
+    @classmethod
     def read_file(self, path):
         file_type = path.split(".")[-1]
 
         readers = {
             "csv": Inventory.read_csv_file,
             "json": Inventory.read_json_file,
+            "xml": Inventory.read_xml_file,
         }
 
         return readers[file_type](path)

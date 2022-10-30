ce9c1b47040a840c5a14636c8f72431fb7f7feda
lucas_almeida <lucas.almdiniz@gmail.com>
2022-10-30 15:40:01 -0300

    feat: Requisito 11
    ao receber pela linha de comando o caminho de um arquivo e o tipo de relatório, devolve o relatório correto

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,21 @@
+import sys
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+IMPORTER = {
+    'csv': CsvImporter,
+    'json': JsonImporter,
+    'xml': XmlImporter,
+}
+
+
 def main():
-    pass
+    if len(sys.argv) < 3:
+        sys.stderr.write('Verifique os argumentos\n')
+    else:
+        _, path, report_type = sys.argv
+        file_type = path.split('.')[-1]
+        invetory = InventoryRefactor(IMPORTER[file_type])
+        print(invetory.import_data(path, report_type), end='')
